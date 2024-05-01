fe5556b6fef97f9e64f6c074e72c7c54416c4be8
wayared <jarecast@espol.edu.ec>
2024-05-01 14:32:31 -0700

    BackEnd port changed

--- Back-End/server.js
+++ Back-End/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.post('/send-email', (req, res) => {
     const { name, email, phone, message, serviceType } = req.body;
