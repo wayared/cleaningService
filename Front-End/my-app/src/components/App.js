d4d4312df2a19796115a7c431840040ccbeec1d3
wayared <jarecast@espol.edu.ec>
2024-05-10 19:52:36 -0700

    components organized

--- Front-End/my-app/src/components/App.js
+++ Front-End/my-app/src/components/App.js
@@ -1,15 +1,15 @@
 // src/App.js
 import React from 'react';
-import Wrap from './Wrap';
-import NavBar from './NavBar';
+import Wrap from '../Wrap';
+import NavBar from '../NavBar';
 import Footer from './Footer';
-import HeroSection from './HeroSection';
-import Introduce from './Introduce' 
-import Portfolio from './Portfolio'
-import WhyChooseUs from './Why';
-import Notification from './Notification';
+import HeroSection from '../HeroSection';
+import Introduce from '../Introduce' 
+import Portfolio from '../Portfolio'
+import WhyChooseUs from '../Why';
+import Notification from '../Notification';
 import Contact from './Contact';
-import Services from './Services';
+import Services from '../Services';
 import About from './About';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
