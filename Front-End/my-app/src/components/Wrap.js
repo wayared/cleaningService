6c1f955be5b781b605a4631d4093755334cd0c6c
wayared <jarecast@espol.edu.ec>
2024-05-13 21:44:27 -0700

    wrap modified

--- Front-End/my-app/src/components/Wrap.js
+++ Front-End/my-app/src/components/Wrap.js
@@ -10,8 +10,8 @@ function Wrap() {
                 <div className="row justify-content-between">
                     <div className="col-12 col-md d-flex align-items-center">
                         <p className="mb-0 phone">
-                            <span className="mailus">Phone no:</span> <a href="tel:+19714270026">+1 (971) 427-0026</a> or
-                            <span className="mailus">email us:</span> <a href="mailto:mcdrcleaning@gmail.com">mcdrcleaning@gmail.com</a>
+                            <span className="mailus">Phone no:</span> <a href="tel:+19714270026">+1 (971) 427-0026</a> or 
+                            <span className="mailus"> email us:</span> <a href="mailto:mcdrcleaning@gmail.com">mcdrcleaning@gmail.com</a>
                         </p>
                     </div>
                     <div className="col-12 col-md d-flex justify-content-md-end">
